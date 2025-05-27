9f247fd556422de587ed7c5f9eb74fa7a06c8628
rkbart <rk_bart@yahoo.com>
2025-05-27 20:28:26 +0800

    updated _footer of home and added about page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,4 @@
-class ChatsController < ApplicationController 
+class ChatsController < ApplicationController
   before_action :authenticate_user!
   def show
     @chat = current_user.chats.find(params[:id])
